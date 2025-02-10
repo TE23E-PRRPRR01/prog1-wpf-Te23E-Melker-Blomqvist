cae604f8606bf72da3ca4653b1639bf7444da919
Melker Blomqvist <melker.blomqvist@elev.ga.ntig.se>
2025-02-10 14:58:13 +0100

    AreaKod

---
+++ AreaApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AreaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickRäkna(object sender, RoutedEventArgs e)
+    {
+        // int bredd = int.Parse(txbBredd.Text);
+        // int höjd = int.Parse(txbHöjd.Text);
+
+        if (!int.TryParse(txbBredd.Text, out int bredd))
+        {
+            txbResultat.Text = "fel ange heltal";
+        }
+
+        if (!int.TryParse(txbHöjd.Text, out int höjd))
+
+
+        int area = bredd * höjd;
+
+        txbResultat.Text = area.ToString();
+    }
+
+    private void KlickRensa(object sender, RoutedEventArgs)
+    {
+        txbBredd.Clear();
+        txbHöjd.Clear();
+        txbResultat.Clear();
+    }
+
+}
