d69269eb09f13813da8a4f64154929dc518b900c
Melker Blomqvist <melker.blomqvist@elev.ga.ntig.se>
2025-02-17 13:20:58 +0100

    Mello

---
+++ MelloApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MelloApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    int antalRöd = 0;
+    int antalBlå = 0;
+    int antalGrön = 0;
+
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void ClickRösta(object sender, RoutedEventArgs e)
+    {
+        if (sender == röd)
+        {
+            antalRöd++;   
+        }
+        else if (sender == blå)
+        {
+            antalBlå++;
+        }
+        else if (sender == grön)
+        {
+            antalGrön++;
+        }
+        else if (sender == gul)
+        {
+            antalGul++;
+        }
+        else if (sender == lila)
+        {
+            antalLila++;
+        }
+
+         txbResultat.Text = $"Röd: {antalRöd}, Blå: {antalBlå}, Grön: {antalGrön}, Gul: {antalGul}, Lila: {antalLila}";
+
+    }
+}
---
+++ SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSlumpa(object sender, RoutedEventArgs e)
+    {
+        if(!int.TryParse(txbMax.Text, out int max))
+        
+        int slumptal = Random.Shared.Next(1, max);
+
+        txbResultat.Text = $"{slumptal}";
+    }
+}
